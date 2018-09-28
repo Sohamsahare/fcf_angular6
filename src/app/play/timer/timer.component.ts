8133bcf2f27050375cf98a9413559e2a6d46aa14
Soham Sahare <sohamsahare@gmail.com>
2018-09-28 11:30:51 +0530

    added a working timer -> changes colours as time decreases -> from green to yellow and finally to red

--- src/app/play/timer/timer.component.ts
+++ src/app/play/timer/timer.component.ts
@@ -5,10 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
+
 export class TimerComponent implements OnInit {
 
   timeInMilliseconds: number;
+  private initalTime: number = 1000 * 20;
   private timeInterval : number = 10;
+  private timerIntervalId : any;
+  private colourIntervalId : any;
+
+  private colours: string[] = ['#FFFF00','#DC3545']
 
   constructor() {
   }
@@ -17,13 +23,37 @@ export class TimerComponent implements OnInit {
     this.startTimer();
   }
 
+  getMinutes() : number{
+    return Math.floor(this.timeInMilliseconds / (1000 * 60)) ;    
+  }
+
+  getSeconds() : number{
+    return Math.floor(((this.timeInMilliseconds % 100000) / 1000)) ;    
+  }
+
+  getMiniSeconds() : number{
+    return Math.round(((this.timeInMilliseconds % 1000) / (10)) * 100) / 100;   
+  }
+
   startTimer(){
-    this.timeInMilliseconds = 1000 * 60;
-    setInterval(() => {
+    this.timeInMilliseconds = this.initalTime;
+    this.timerIntervalId =  setInterval(() => {
       this.timeInMilliseconds -= this.timeInterval;
+      if(this.timeInMilliseconds <= 0 ){
+        this.stopTimer();
+      }
     },this.timeInterval);
-    if(this.timeInMilliseconds <= 0 ){
-      return;
+    let colourId = 0;
+    this.colourIntervalId = setInterval(() =>{
+      let bgColour = document.getElementById('timer-box');
+      bgColour.style.backgroundColor = this.colours[colourId] ;
+      colourId++;  
     }
+    ,(this.initalTime/3));
+  }
+
+  stopTimer(){
+    clearInterval(this.timerIntervalId);
+    clearInterval(this.colourIntervalId);
   }
 }
