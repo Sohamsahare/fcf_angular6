252b94d9a2576060ba7564cc9856a3321081a8f0
Soham Sahare <sohamsahare@gmail.com>
2018-09-28 18:06:35 +0530

    initial colour change using aferviewinit

--- src/app/play/card/card.component.ts
+++ src/app/play/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, DoCheck, AfterContentChecked } from '@angular/core';
 import { Card } from './card.model';
 
 @Component({
@@ -6,13 +6,29 @@ import { Card } from './card.model';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, AfterContentChecked {
   @Input() card : Card;
 
   constructor() { }
 
   ngOnInit() {
+    // this.toChangeColor();
     // console.log(this.card.id);
+  }
+  
+  // ngAfterContentChecked(){
+  //   this.toChangeColor();
+  // }
+
+  ngAfterViewInit() {
+    let elem = document.getElementById("card_" + this.card.id);
+    elem.style.backgroundColor = (this.card.isGreen)?'green':'white';
+    // console.log("elem", elem);
+  }
+
+  toChangeColor(){
+    let elem = document.getElementById("card_"+ this.card.id);
+    console.log("elem", elem);
   }
 
 }
--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -9,22 +9,42 @@ import { Card } from './card/card.model';
 
 export class PlayComponent implements OnInit {
 
-  cardcount : number = 4;
-  cards: Card[] = new Array(this.cardcount).fill({id:0 ,isGreen:false});
+  cardcount: number = 4;
+  cards: Card[] = new Array(this.cardcount).fill({ id: 0, isGreen: false });
+  randomCardIntervalId: any;
+  private initialRandomTimeInMs = 2000;
+  randomTimeInMs: number = this.initialRandomTimeInMs;
+
+  private lastRandomNumber: number = 1;
 
   constructor() {
     this.initialiseCards();
-    console.log(this.cards[1].id);
   }
-  
+
   ngOnInit() {
-
+    this.randomCardIntervalId = setInterval(
+      () => {
+        this.cards[this.lastRandomNumber - 1].isGreen = false;
+        let randomId: number = this.randomCard();
+        this.cards[randomId - 1].isGreen = true;
+      },
+      this.randomTimeInMs);
   }
 
-  initialiseCards(){
+  initialiseCards() {
     for (let card = 0; card < this.cards.length; card++) {
-      this.cards[card] = { id: card+1, isGreen:false};
+      this.cards[card] = { id: card + 1, isGreen: (card == 0) ? true : false};
+      // console.log(this.randomCard(1,this.cardcount,this.lastRandomNumber));
     }
+  }
+
+  randomCard() {
+    let randomNumber: number = Math.floor(Math.random() * (this.cardcount) + 1);
+    while (randomNumber == this.lastRandomNumber) {
+      randomNumber = Math.floor(Math.random() * (this.cardcount) + 1);
+    }
+    this.lastRandomNumber = randomNumber;
+    return randomNumber;
   }
 
 }
