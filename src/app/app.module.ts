479637266fb29918434aa573f06d5d20cbf57810
Soham Sahare <sohamsahare@gmail.com>
2018-09-28 14:11:29 +0530

    added scores view -> fetches data from scores.json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './landing/button/button.component';
@@ -9,6 +8,9 @@ import { NotFoundComponent } from './notfound/notfound.component';
 import { PlayComponent } from './play/play.component';
 import { ScoresComponent } from './scores/scores.component';
 import { TimerComponent } from './play/timer/timer.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ScoresService } from '../services/scores.service';
+
 
 @NgModule({
   declarations: [
@@ -18,13 +20,14 @@ import { TimerComponent } from './play/timer/timer.component';
     NotFoundComponent,
     PlayComponent,
     ScoresComponent,
-    TimerComponent
+    TimerComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ScoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/scores/scores.component.ts
+++ src/app/scores/scores.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Scores } from './scores.model';
+import { ScoresService } from '../../services/scores.service';
 
 @Component({
   selector: 'app-scores',
@@ -8,14 +9,13 @@ import { Scores } from './scores.model';
 })
 export class ScoresComponent implements OnInit {
 
-  scores : Scores[] = [ 
-    { id: 1, playedAt: "12:00 PM", score: 9999 },
-    { id: 2, playedAt: "1:00 PM", score: 999 },
-    { id: 3, playedAt: "2:00 PM", score: 99 }
-  ] ;
-  constructor() { }
+  scores : Scores[] ;
+  constructor(private scoresService : ScoresService) { }
 
   ngOnInit() {
+    this.scoresService.getScores().subscribe((scoresDb : Scores[]) => {
+      this.scores = scoresDb;
+    })
   }
 
 }
