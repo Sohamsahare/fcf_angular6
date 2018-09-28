0985e7a3a824812633c76cf8c2b04fab148e5851
Soham Sahare <sohamsahare@gmail.com>
2018-09-28 11:31:30 +0530

    Added a initial scores table -> used data from class -> added json db file but didnt use it

--- src/app/scores/scores.component.ts
+++ src/app/scores/scores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Scores } from './scores.model';
 
 @Component({
   selector: 'app-scores',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoresComponent implements OnInit {
 
+  scores : Scores[] = [ 
+    { id: 1, playedAt: "12:00 PM", score: 9999 },
+    { id: 2, playedAt: "1:00 PM", score: 999 },
+    { id: 3, playedAt: "2:00 PM", score: 99 }
+  ] ;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/scores/scores.model.ts
@@ -0,0 +1,5 @@
+export class Scores{
+    id:number;
+    playedAt:string;
+    score:number;
+}
